cabcc9bbf0149435f9aaeccb17ab1d62e9f67151
Ednardo Gomes <ednardo@email.com>
2022-03-29 22:04:57 -0300

    Montando a estrutura básica do projeto.

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
 export function App() {
   return (
     <div className="App">
-      <h1>Activit List</h1>
+      <header>
+        <span>Activity List</span>
+        <div className="user">
+          <h2>Ednardo Gomes</h2>
+          <span>Bem-vindo(a) ao Activity List</span>
+        </div>
+      </header>
+      <main>
+        <div className="cards">
+          <h1>Card1</h1>
+          <h1>Card2</h1>
+        </div>
+        <div className="input">
+          <h1>Minhas Atividades</h1>
+          <input type="text" />
+          <button type="button">Adicionar</button>
+        </div>
+      </main>
     </div>
   );
 }
